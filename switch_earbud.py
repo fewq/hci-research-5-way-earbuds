cefe1db82876c7191b3e8e2566d7044a8d55b995
thaddeusphua <thaddeus.phua@gmail.com>
2019-11-04 21:42:43 +0900

    added comments to switch_earbud

--- switch_earbud.py
+++ switch_earbud.py
@@ -2,18 +2,18 @@ import RPi.GPIO as GPIO
 import time
 
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(40, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(38, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(37, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(35, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(33, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(40, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # left
+GPIO.setup(38, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # center
+GPIO.setup(37, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # up
+GPIO.setup(35, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # right
+GPIO.setup(33, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)	# down
 
 while True:
-	time.sleep(0.1)
+	time.sleep(0.1) # set a delay between commands
 	if GPIO.input(40) == GPIO.HIGH:
 		print("LEFT")
 	elif GPIO.input(38) == GPIO.HIGH:
-		print("CENTRE")
+		print("CENTER")
 	elif GPIO.input(37) == GPIO.HIGH:
 		print("UP")
 	elif GPIO.input(35) == GPIO.HIGH:

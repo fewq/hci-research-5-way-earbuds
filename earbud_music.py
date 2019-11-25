8b16ec4ef6030727c17ae6f09c55d7a0fec9e686
thaddeusphua <thaddeus.phua@gmail.com>
2019-11-25 21:24:22 +0900

    cleaned up print statements and volume adjustment

--- earbud_music.py
+++ earbud_music.py
@@ -21,52 +21,48 @@ index = 0
 
 first_song_played = False # this is the intial setup
 pause_state = False # toggle between pause and unpause
-volume = 0.3 #volume is between 0 and 1
+volume = 0.69 #volume is between 0 and 1
 
 while True:
 	time.sleep(0.5) # set a delay between commands
 	if GPIO.input(left) == GPIO.HIGH:
-		print("LEFT")
 		index = (index-1)%3 #go back 1 song
 		mixer.music.load(playlist[index])
 		mixer.music.play()
-		print(index)
+		print("LEFT; ", playlist[index])
 	elif GPIO.input(center) == GPIO.HIGH:
-		print("CENTER")
 		if first_song_played == False:
 			mixer.music.load(playlist[index])
 			mixer.music.play()
 			first_song_played = True
+			print("CENTER; play")
 		elif first_song_played == True:
 			if pause_state == False:
 				mixer.music.pause()
 				pause_state = True
-				print("paused")
+				print("CENTER; paused")
 			elif pause_state == True:
 				mixer.music.unpause()
 				pause_state = False
-				print("unpaused")
+				print("CENTER; unpaused")
 	elif GPIO.input(up) == GPIO.HIGH:
-		print("UP")
-		new_volume = volume + 0.3 
+		new_volume = volume + 0.3
 		if new_volume > 1: # check that the new volume is within bounds
 			pass
 		else:
 			volume = new_volume
 			mixer.music.set_volume(volume)
-		print("Volume set to: ", volume)
+		print("UP; Volume: {:.2f}".format(volume))
 	elif GPIO.input(right) == GPIO.HIGH:
-		print("RIGHT")
 		index = (index+1)%3 #go forward 1 song
 		mixer.music.load(playlist[index])
 		mixer.music.play()
-		print(index)
+		print("RIGHT; ", playlist[index])
 	elif GPIO.input(down) == GPIO.HIGH:
-		print("DOWN")
 		new_volume = volume - 0.3
 		if new_volume < 0: # check that the new volume is within bounds
 			pass
 		else:
 			volume = new_volume
 			mixer.music.set_volume(volume)
-		print("Volume set to: ", volume)
+		print("DOWN; Volume: {:.2f}".format(volume))
